6bcaf704fd884783f3977da54985611706a89337
Adrian Gallus <adrian.gallus@web.de>
2025-05-06 12:31:07 +0200

    refactor: doc -> docs

--- src/lazysignals/__init__.py
+++ src/lazysignals/__init__.py
@@ -1,2 +1,8 @@
 
-from .signals import Signal, effect, derived
+"""
+A library that allows you to run effects whenever state changes.
+It employs dependency discovery and updates are lazy.
+"""
+
+from ._signals import Signal, effect, derived
+__all__ = ["Signal", "effect", "derived"]
