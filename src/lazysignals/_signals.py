03886926615788371c0e489135e9becc2cc3d490
Adrian Gallus <adrian.gallus@web.de>
2025-05-07 16:05:53 +0200

    feat: simplify Effect and add some documentation

--- src/lazysignals/_signals.py
+++ src/lazysignals/_signals.py
@@ -21,6 +21,13 @@ class SingletonMeta(type):
 
 # unfortunately the .add method does not return its effect
 def is_added(s, x):
+    """
+    Adds an element to a set and returns if it was not already present.
+    
+    :param: ``s``: The set to add the element to.
+    :param: ``x``: The element to add.
+    :returns: ``True`` if ``x`` was not in ``s``, ``False`` otherwise.
+    """
     if x not in s:
         s.add(x)
         return True
@@ -34,17 +41,40 @@ class Updated(metaclass=SingletonMeta):
         self._signals = set()
         self._updated = set()
 
-    def enter_signal(self, signal):
+    def enter(self, signal):
         self._signals.add(signal)
 
-    def leave_signal(self, signal):
+    def leave(self, signal):
         self._signals.remove(signal)
         # cleanup when all signales propagated
         if not self._signals:
             self._updated = set()
 
     def submit(self, updated):
-        return is_added(self._updated, updated)
+        if self._signals:
+            return is_added(self._updated, updated)
+        return True
+
+
+class Dependent(metaclass=SingletonMeta):
+
+    def __init__(self):
+        self._effects = []
+
+    @property
+    def is_set(self):
+        return len(self._effects) > 0
+
+    def get(self, dependency):
+        effect = self._effects[-1]
+        fresh = effect.add_dependency(dependency)
+        return fresh, effect
+    
+    def pop(self):
+        self._effects.pop()
+
+    def push(self, effect):
+        self._effects.append(effect)
 
 
 # run updates (but only once per change)
@@ -60,40 +90,28 @@ class Effect():
     def update(self):
         updated = Updated()
         if updated.submit(self):
-            effect(self._fn, effect=self)
-
-
-class Dependent(metaclass=SingletonMeta):
-
-    def __init__(self):
-        self._effects = []
-
-    def pop(self):
-        self._effects.pop()
-
-    def push(self, effect):
-        self._effects.append(effect)
-
-    @property
-    def is_set(self):
-        return len(self._effects) > 0
-
-    def get(self, dependency):
-        effect = self._effects[-1]
-        fresh = effect.add_dependency(dependency)
-        return fresh, effect
+            Dependent().push(self)
+            try:
+                return self._fn()
+            except:
+                raise
+            finally:
+                Dependent().pop()
 
 
 class Signal:
+    """A container to hold a reactive value."""
 
     def __init__(self, value=None):
         self._value = value
         self._dependents = [] # NOTE must preserve order (may use dict instead of list) to ensure that each (single) effect update happens only after all dependencies already updated
 
     def __str__(self):
+        """get a reactive string representation of the contained value"""
         return str(self.value)
 
     def __repr__(self):
+        """get a (nonreactive) string representation of the container"""
         return f"Signal({self._value})"
 
     @property
@@ -116,33 +134,37 @@ class Signal:
         self._value = value
         exceptions = []
         updated = Updated()
-        updated.enter_signal(self)
+        updated.enter(self)
         for dependent in list(self._dependents):
             try:
                 dependent.update()
             except Exception as e:
                 exceptions.append(e)
-        updated.leave_signal(self)
+        updated.leave(self)
         if len(exceptions) > 0:
             raise Exception(*exceptions)
 
 
 # NOTE may be used as decorator
-def effect(fn, effect=None):
-    Dependent().push(effect or Effect(fn))
-    try:
-        fn()
-    except:
-        raise
-    finally:
-        Dependent().pop()
+def effect(fn):
+    """
+    Run ``fn()`` whenever (relevant) state changes.
+
+    :param: ``fn``: The function to run on state changes.
+    :returns: The return value of ``fn()``.
+    """
+    return Effect(fn).update()
 
 
 # NOTE may be used as decorator, similar to @property
 def derived(fn):
+    """
+    Define a new signal whose value is computed by ``fn()``.
+    
+    :param: ``fn``: A function without arguments that returns a value.
+    :returns: A new ``Signal``.
+    """
     derived = Signal()
     effect(lambda: derived.set(fn()))
     return derived
-
-
 
